cc4dabd9203ee5f91d85d99311784993ac506a90
jualtmey <julian.altmeyer@gmail.com>
2017-05-12 16:49:58 +0200

    added HeightMapTest

---
+++ HeightMapTest/Assets/Scripts/HeightMapManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapManager : MonoBehaviour {
+
+	private float[] data;
+	private ushort[] rawData;
+	private Color[] colorData;
+	private bool pendingData;
+
+	public string imageName;
+	private Texture2D texture;
+
+	// mesh kann nicht mehr als 65000 vertices haben
+    private const int downsampleSize = 8;
+    private const float depthScale = 0.1f; // 0.02 für Bilder
+
+	private const int ORIGINAL_WIDTH = 1920;
+	private const int ORIGINAL_HEIGHT = 1080;
+	public const int WIDTH = ORIGINAL_WIDTH / downsampleSize;
+	public const int HEIGHT = ORIGINAL_HEIGHT / downsampleSize;
+
+	// timer
+    private float timer;
+    public float updateInterval = 1;
+
+	void Start () {
+		data = new float[WIDTH * HEIGHT];
+		rawData = new ushort[ORIGINAL_WIDTH * ORIGINAL_HEIGHT];
+		colorData = new Color[WIDTH * HEIGHT] ;
+
+		texture = Resources.Load(imageName) as Texture2D;
+	}
+	
+	void OnGUI() {
+		//GUI.Box(new Rect(10,10,100,90), "Loader Menu");
+		//GUI.DrawTexture(new Rect(0, 0, 500, 200), texture);
+	}
+	private bool firstTime = true;
+	void Update () {
+		if (timer > updateInterval || firstTime) {
+			firstTime = false;
+			pendingData = true;
+
+			//randomData();
+			rgbData();
+
+			downsampleAndScaleData();
+
+			timer = 0;
+		}
+        timer += Time.deltaTime;
+	}
+
+	private void randomData() {
+		for (int y = 0; y < ORIGINAL_HEIGHT; y ++)
+		{
+			for (int x = 0; x < ORIGINAL_WIDTH; x ++)
+			{
+				// rawData[(y * _Width) + x] = (ushort) (x + y);
+				rawData[(y * ORIGINAL_WIDTH) + x] = (ushort) (Random.value * 10);
+				// rawData[(y * _Width) + x] = (ushort) ((10 * Mathf.Sin(Mathf.Pow(x, 2) + Mathf.Pow(y, 2)) / 10));
+				// rawData[(y * _Width) + x] = (ushort) ((Mathf.Sin(Mathf.Deg2Rad * (5f * x)) * Mathf.Cos(Mathf.Deg2Rad * (5f * y)) / 5f) * 5f) ;
+			}
+		}
+	}
+
+	private void rgbData() {
+		for (int y = 0; y < ORIGINAL_HEIGHT; y ++)
+		{
+			for (int x = 0; x < ORIGINAL_WIDTH; x ++)
+			{
+				Color pixel = texture.GetPixel(x, y);
+				// int red = (int) (pixel.r * 255);
+				// int green = (int) (pixel.g * 255);
+				// int blue = (int) (pixel.b * 255);
+				// int rgb = (red << 16) + (green << 8) + blue;
+
+				// rawData[(y * ORIGINAL_WIDTH) + x] = (ushort) (rgb / 3894); // geteilt durch 3894, damit Wert zwischen 0 und 65535 liegt
+				rawData[(y * ORIGINAL_WIDTH) + x] = (ushort) (pixel.r * 1000 + pixel.g * 100 + pixel.b * 10);
+
+				if (x % downsampleSize == 0 && y % downsampleSize == 0) {
+					colorData[((y / downsampleSize) * WIDTH) + (x / downsampleSize)] = pixel;
+				}
+			}
+		}
+	}
+
+	private void downsampleAndScaleData() {
+		for (int y = 0; y < ORIGINAL_HEIGHT; y += downsampleSize)
+        {
+            for (int x = 0; x < ORIGINAL_WIDTH; x += downsampleSize)
+            {
+				int indexX = x / downsampleSize;
+				int indexY = y / downsampleSize;
+				int smallIndex = (indexY * (ORIGINAL_WIDTH / downsampleSize)) + indexX;
+				
+				float avg = GetAvg(rawData, x, y, ORIGINAL_WIDTH, ORIGINAL_HEIGHT);
+				// float avg = rawData[(y * ORIGINAL_WIDTH) + x];
+				
+				avg = avg * depthScale;
+
+				if (avg == 0) avg = 1;
+
+				data[smallIndex] = avg;
+			}
+		}
+	}
+
+	private float GetAvg(ushort[] rawData, int x, int y, int width, int height)
+    {
+        float sum = 0f;
+        
+        for (int y1 = y; y1 < y + downsampleSize; y1++)
+        {
+            for (int x1 = x; x1 < x + downsampleSize; x1++)
+            {
+                int fullIndex = (y1 * width) + x1;
+                sum += rawData[fullIndex];
+            }
+        }
+
+        return sum / (downsampleSize * downsampleSize);
+    }
+
+	public bool HasPendingData() {
+		return pendingData;
+	}
+
+	public float[] GetData() {
+		pendingData = false;
+		return data;
+	}
+
+	public Color[] GetColorData() {
+		return colorData;
+	}
+
+}
---
+++ HeightMapTest/Assets/Scripts/HeightMapView.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapView : MonoBehaviour {
+
+    public HeightMapManager heightMapManager;
+    public ViewMode viewMode;
+
+    // mesh fields
+	private Mesh _Mesh;
+    private Vector3[] _Vertices;
+    private Vector2[] _UV;
+    private int[] _Triangles;
+
+    // pillar fields
+    public GameObject pillarPrefab;
+    private GameObject[] pillars;
+    
+    private int width;
+    private int height;
+
+    // enum
+    public enum ViewMode {
+        Mesh,
+        Pillar
+    }
+
+    void Start()
+    {
+        width = HeightMapManager.WIDTH;
+        height = HeightMapManager.HEIGHT;
+
+        if (viewMode == ViewMode.Mesh) {
+            // Downsample to lower resolution
+            CreateMesh(width, height);
+        }
+        else {
+            // Downsample to lower resolution
+            CreatePillars(width, height);
+        }
+    }
+
+    void CreateMesh(int width, int height)
+    {
+        _Mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = _Mesh;
+        Vector3 position = GetComponent<Transform>().position;
+
+        _Vertices = new Vector3[width * height];
+        _UV = new Vector2[width * height];
+        _Triangles = new int[6 * ((width - 1) * (height - 1))];
+
+        int triangleIndex = 0;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int index = (y * width) + x;
+
+                _Vertices[index] = new Vector3(x + position.x, position.y, -1 * (y + position.z));
+                _UV[index] = new Vector2(((float)x / (float)width), ((float)y / (float)height));
+
+                // Skip the last row/col
+                if (x != (width - 1) && y != (height - 1))
+                {
+                    int topLeft = index;
+                    int topRight = topLeft + 1;
+                    int bottomLeft = topLeft + width;
+                    int bottomRight = bottomLeft + 1;
+
+                    _Triangles[triangleIndex++] = topLeft;
+                    _Triangles[triangleIndex++] = topRight;
+                    _Triangles[triangleIndex++] = bottomLeft;
+                    _Triangles[triangleIndex++] = bottomLeft;
+                    _Triangles[triangleIndex++] = topRight;
+                    _Triangles[triangleIndex++] = bottomRight;
+                }
+            }
+        }
+
+        _Mesh.vertices = _Vertices;
+        _Mesh.uv = _UV;
+        _Mesh.triangles = _Triangles;
+        _Mesh.RecalculateNormals();
+		//GetComponent<MeshCollider>().sharedMesh = _Mesh;
+    }
+
+    void CreatePillars(int width, int height)
+    {
+        pillars = new GameObject[width * height];
+        Vector3 position = GetComponent<Transform>().position;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int index = (y * width) + x;
+                float relativeX = x + position.x;
+                float relativeZ = y + position.z;
+
+                GameObject pillar = Instantiate(pillarPrefab, new Vector3(relativeX, position.y, relativeZ * -1), Quaternion.identity);
+                pillars[index] = pillar;
+            }
+        }
+
+    }
+
+    void Update()
+    {
+		if (heightMapManager.HasPendingData()) {
+			RefreshData(heightMapManager.GetData());
+			// GetComponent<MeshCollider>().sharedMesh = _Mesh;
+		}
+    }
+    
+    private void RefreshData(float[] data)
+    {
+        Vector3 position = GetComponent<Transform>().position;
+        Color[] colorData = heightMapManager.GetColorData();
+
+        for (int i = 0; i < data.Length; i++)
+        {    
+            float dataValue = data[i];
+            if (viewMode == ViewMode.Mesh) {
+                _Vertices[i].y = dataValue;
+            }
+            else {
+                Transform pillarTransform = pillars[i].GetComponent<Transform>();
+                pillarTransform.localScale = new Vector3(1, dataValue, 1);
+                pillarTransform.position = new Vector3(pillarTransform.position.x, position.y + ((float) (dataValue / 2)), pillarTransform.position.z);
+
+                Renderer rend = pillars[i].GetComponent<Renderer>();
+                rend.material.color = colorData[i];
+            }
+        }
+        if (viewMode == ViewMode.Mesh) {
+            _Mesh.vertices = _Vertices;
+            _Mesh.uv = _UV;
+            _Mesh.triangles = _Triangles;
+            _Mesh.RecalculateNormals();
+        }
+
+    }
+
+}
